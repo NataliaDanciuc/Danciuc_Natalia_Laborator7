3431d117b79dd0f45a62ce101d11cdba3d5ca1c1
NataliaDanciuc <nataliadanciuc112@gmail.com>
2022-12-13 17:11:05 +0200

    lab10

--- Models/ShopList.cs
+++ Models/ShopList.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using SQLite;
+using SQLiteNetExtensions.Attributes;
 
 namespace Danciuc_Natalia_Laborator7.Models
 {
@@ -14,5 +15,9 @@ namespace Danciuc_Natalia_Laborator7.Models
         [MaxLength(250), Unique]
         public string Description { get; set; }
         public DateTime Date { get; set; }
+
+        [ForeignKey(typeof(Shop))]
+        public int ShopID { get; set; }
+
     }
 }
